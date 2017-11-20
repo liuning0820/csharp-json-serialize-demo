80af43abce2ab6aaf27ef4845541f46fb8ad1f04
liuning0820 <liuning0820@outlook.com>
2017-11-20 10:44:30 +0800

    Serializes using Newtonsoft.Json.JsonConvert

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 using System.IO;
 
 //Need to add reference to System.Web.Extension.dll
-using System.Web.Script.Serialization; 
+using System.Web.Script.Serialization;
+
+//Another way to serialize json into C# using newtonsoft.Json
+using Newtonsoft.Json;
 
 namespace csharp_json_serialize_demo
 {
@@ -17,12 +20,18 @@ namespace csharp_json_serialize_demo
 
             String jsonString = File.ReadAllText("demo.json");
 
+            ////Serializes using JavaScriptSerializer
             JavaScriptSerializer serializer = new JavaScriptSerializer();
 
             Person person = serializer.Deserialize<Person>(jsonString);
 
             Console.WriteLine(person.ToString());
-            
+
+            //Serializes using Newtonsoft.Json.JsonConvert
+            Person person2 = JsonConvert.DeserializeObject<Person>(jsonString);
+
+            Console.WriteLine(person2.ToString());
+
         }
     }
 
