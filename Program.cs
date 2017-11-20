d982c73adc13d2d79b24cfb8ccdac14a7725c845
liuning0820 <liuning0820@outlook.com>
2017-11-20 10:33:40 +0800

    Serialize Json file in csharp using JavaScriptSerializer class

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
+//Need to add reference to System.Web.Extension.dll
+using System.Web.Script.Serialization; 
 
 namespace csharp_json_serialize_demo
 {
@@ -10,6 +14,28 @@ namespace csharp_json_serialize_demo
     {
         static void Main(string[] args)
         {
+
+            String jsonString = File.ReadAllText("demo.json");
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            Person person = serializer.Deserialize<Person>(jsonString);
+
+            Console.WriteLine(person.ToString());
+            
+        }
+    }
+
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0} \n Age: {1}",Name,Age);
         }
     }
 }
