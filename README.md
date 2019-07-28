6a881c4e1376f661592886dab90a6de163636b8d
nliu6 <ning.liu4@dxc.com>
2019-07-28 11:16:48 +0800

    dotnet core 3.0 support build in Json serialization in namespace System.Text.Json.

---
+++ README.md
@@ -0,0 +1,14 @@
+# JSON SERIALIZE
+
+Two Ways to do Json Serialiation.
+
+## Extention or External Package
+
+- Add Reference to System.Web.Extension.dll
+- Newtonsoft.Json Package
+
+See example in [dotnet-framework45](./dotnet-framework45/Program.cs)
+
+## Build In Support in DotNet Core 3.0
+
+See example in [dotnet-core](./dotnet-core\Program.cs)
---
+++ dotnet-core/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace netcore30_buildin_json_serialize_demo
+{
+
+
+
+        class WeatherForecast
+        {
+            public DateTimeOffset Date { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var w = new WeatherForecast() { Date = DateTime.Now, TemperatureC = 30, Summary = "Hot" };
+                Console.WriteLine(JsonSerializer.Serialize<WeatherForecast>(w));
+            }
+        }
+
+}
